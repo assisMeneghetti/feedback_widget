cf9680ab5ebabeede37e1b9c64706d5e883b64ee
assisMeneghetti <assismfabri@hotmail.com>
2022-05-13 19:35:03 -0300

    first commit

---
+++ web/src/components/Widget.tsx
@@ -0,0 +1,29 @@
+import { ChatTeardropDots } from "phosphor-react";
+// import { useState } from "react";
+import { Popover } from '@headlessui/react'
+import { WidgetForm } from "./WidgetForm";
+// import { CloseButton } from "./CloseButton";
+export function Widget () {
+  // const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
+  // function toggleWidgetVisibility () {
+  //   setIsWidgetOpen(!isWidgetOpen);
+  // }
+
+  return (
+    <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
+      {/* {isWidgetOpen && <p>Hello World</p>} */}
+      <Popover.Panel>
+        <WidgetForm />
+      </Popover.Panel>
+      {/* <CloseButton /> */}
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+        <ChatTeardropDots className="w-6 h-6"/>
+
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">Feedback
+          <span className="pl-2"></span>
+        </span>
+      </Popover.Button>
+    </Popover>
+  )
+}
